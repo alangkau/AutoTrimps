c0070e96034f80dd093d1b8aedbe31209fe897f5
Adam Langkau <alangkau@gmail.com>
2021-02-17 18:00:50 +0100

    changed user js file

--- .user.js
+++ .user.js
@@ -1,13 +1,13 @@
 // ==UserScript==
-// @name         AutoTrimps-genBTC
+// @name         my autotrimps
 // @version      2.1.6.9-genbtc-3-23-2018
-// @namespace    https://github.com/alangkau/AutoTrimps
-// @updateURL    https://github.com/alangkau/AutoTrimps/.user.js
+// @namespace    https://alangkau.github.io/AutoTrimps/
+// @updateURL    https://alangkau.github.io/AutoTrimps//.user.js
 // @description  Automate all the trimps!
 // @author       zininzinin, spindrjr, Ishkaru, genBTC
 // @include      *trimps.github.io*
 // @include      *kongregate.com/games/GreenSatellite/trimps
-// @connect      *github.com/alangkau/AutoTrimps*
+// @connect      *alangkau.github.io/AutoTrimps*
 // @connect      *trimps.github.io*
 // @connect      self
 // @grant        none
@@ -16,7 +16,7 @@
 var script = document.createElement('script');
 script.id = 'AutoTrimps-script';
 //This can be edited to point to your own Github Repository URL.
-script.src = 'https://github.com/alangkau/AutoTrimps/AutoTrimps2.js';
+script.src = 'https://alangkau.github.io/AutoTrimps/AutoTrimps2.js';
 //script.setAttribute('crossorigin',"use-credentials");
 script.setAttribute('crossorigin', "anonymous");
 document.head.appendChild(script);
